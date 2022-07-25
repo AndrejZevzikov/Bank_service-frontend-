242630c6319916238f65ae47875ea0defc8d1124
Andrej Zevzikov <andrejzevz13@gmail.com>
2022-07-25 22:54:46 +0300

    Start developing loans page

--- src/app/components/loans/loans.component.ts
+++ src/app/components/loans/loans.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Loan } from 'src/app/entities/loan';
 import { AuthUserService } from 'src/app/sevices/auth-user.service';
 
 @Component({
@@ -12,11 +13,25 @@ export class LoansComponent implements OnInit {
 
   apiUrl: string = "http://localhost:8080/customer"
 
-  constructor(private authUserService: AuthUserService) { }
+  constructor(private authUserService: AuthUserService, private httpClient: HttpClient) { }
+  headers: HttpHeaders = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authUserService.getJwt() });
+  loans: Loan[] = [];
 
   ngOnInit(): void {
     this.authUserService.checkAccesToken();
+    this.getLoans();
+  }
 
+  onPrinterClick(loan: any) {
+    console.log(loan);
+  }
+
+  getLoans() {
+    this.httpClient.get<Loan[]>("http://localhost:8081/" + "loans", { headers: this.headers }).subscribe(
+      (result) => {
+        this.loans = result;
+      }
+    );
   }
 }
 
--- src/app/entities/loan.ts
+++ src/app/entities/loan.ts
@@ -1,7 +1,10 @@
 export interface Loan {
-
+id?:number;
 type:string;
 status:string;
 signDate:string;
 monthToReturn:number;
+amount?:number;
+currencyCode?:number;
+
 }
